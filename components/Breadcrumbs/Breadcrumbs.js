819158982e35f54de7112fbf042dcc637b4b96c2
robinrabauke <robin.mesaric@adac-camping.de>
2021-11-19 18:09:56 +0100

    Clean up

---
+++ components/Breadcrumbs/Breadcrumbs.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Breadcrumbs = () => (
+  <div class="text-sm breadcrumbs">
+    <ul>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>Documents</a>
+      </li>
+      <li>Add Document</li>
+    </ul>
+  </div>
+);
